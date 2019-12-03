0a65bfb457753cc1c20cebaea56c8e0e39143bf3
kim_minju08 <kim_minju08@elandsystems.com>
2019-12-03 18:22:43 +0900

    Init project

---
+++ src/main/java/com/alswn/pay/PayTestApplication.java
@@ -0,0 +1,13 @@
+package com.alswn.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alswn/pay/PayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alswn.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
